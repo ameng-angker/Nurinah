7e2541c6983eaf89771e4512f1d30b522093a28d
Gendar124 <161517312+Gendar124@users.noreply.github.com>
2024-10-11 22:12:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -475,8 +475,25 @@ function getVLESSConfig(userID, hostName) {
 	const vlessMain4 = `vless://${userID}\u0040104.17.3.81:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#EDUKASI`
 	const vlessMain5 = `vless://${userID}\u0040Www.sushiroll.co.id:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#SUSHIROLL`
 	const vlessMain6 = `vless://${userID}\u0040zoomcares.zoom.us:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#CONFERENSI`
+	const vlessMain7 = `vless://${userID}\u0040162.159.138.78:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#BYU`
 	return `
 ################################################################
+
+================================================================
+           SILAHKAN COPY AKUN YG MAU ANDA GUNAKAN
+================================================================
+
+
+WHATSAPP ID: 083117530527
+    https://wa.link/d982tb
+
+
+ COPY DIBAWAH UNTUK AMBIL AKUN ANDA LAGI
+ COPY : http://${hostName}/${userID}
+================================================================
+                   COPY DI BAWAH
+================================================================
+
 AKUN TANPA EDIT
 ---------------------------------------------------------------
 ${vlessMain}
@@ -508,9 +525,12 @@ ${vlessMain6}
 AKUN GAME
 ---------------------------------------------------------------
 ${vlessMain3}
-
 ---------------------------------------------------------------
 
+AKUN BYU
+---------------------------------------------------------------
+${vlessMain7}
+---------------------------------------------------------------
 
 
 ################################################################
@@ -532,16 +552,13 @@ clash-meta
       host: ${hostName}
 ---------------------------------------------------------------
 ################################################################
-HUB PENYEDIA LAYANAN KLIK DI BAWAH
 
 
-WHATSAPP ID: 083117530527
-    https://wa.link/d982tb
 
-================================================================
- COPY DI BAWAH UNTUK CEK AKUN KEMBALI
-================================================================
- COPY :  ${hostName}/${userID}
+
+
+             SELAMAT BERSENANG-SENANG
+
 
 `;
 }
