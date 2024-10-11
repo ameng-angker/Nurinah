c49ac3c55593d2ae9c2fbc5312cd61672a1589d2
Gendar124 <161517312+Gendar124@users.noreply.github.com>
2024-10-11 16:25:32 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
-let proxyIPs = ['172.232.234.33:587'];
+let proxyIPs = ['172.232.238.169:443'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 let proxyPort = proxyIP.includes(':') ? proxyIP.split(':')[1] : '443';
 
