56442b0caf19353fdb3b01a17d90406549e3f835
ameng-angker <124005068+ameng-angker@users.noreply.github.com>
2024-10-11 15:24:17 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
-let proxyIPs = ['103.180.161.123:587'];
+let proxyIPs = ['172.232.234.33:587'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 let proxyPort = proxyIP.includes(':') ? proxyIP.split(':')[1] : '443';
 
